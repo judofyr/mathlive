ff13f1e685997f8f439069634349203787a089fd
Magnus Holm <judofyr@gmail.com>
2015-07-29 17:31:06 +0200

    We only need C++11 for now

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ FREETYPE2="vendor/freetype2"
 CFLAGS="-I$FREETYPE2/include"
 LDFLAGS="-Lbuild -lfreetype"
 
-cc -std=c++14 main.cpp $CFLAGS $LDFLAGS -o build/main
+cc -std=c++11 main.cpp $CFLAGS $LDFLAGS -o build/main
 
